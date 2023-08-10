1307ec3969e5f094753690badf8aba41b85e2344
diepquanghuy <diepquanghuy.20051999@gmail.com>
2023-08-10 21:02:28 +0700

    new file:   .gitignore 	new file:   CHANGELOG.md 	new file:   analysis_options.yaml 	new file:   bin/luyen_tap.dart 	new file:   lib/luyen_tap.dart 	new file:   pubspec.lock 	new file:   pubspec.yaml 	new file:   test/luyen_tap_test.dart

---
+++ bin/luyen_tap.dart
@@ -0,0 +1,6 @@
+import 'package:luyen_tap/luyen_tap.dart' as luyen_tap;
+
+void main(List<String> arguments) {
+  print('Hello world: ${luyen_tap.calculate()}!');
+  print('a');
+}
---
+++ test/luyen_tap_test.dart
@@ -0,0 +1,8 @@
+import 'package:luyen_tap/luyen_tap.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
