33293dd7caaf193e1bcb0517172c7d47f592668b
Diep Quang Huy <diepquanghuy.1999@gmail.com>
2024-03-02 17:03:34 +0700

    modified:   bin/luyen_tap.dart

--- bin/luyen_tap.dart
+++ bin/luyen_tap.dart
@@ -1,4 +1,6 @@
+import 'package:luyen_tap/data_structure_and_algorithms/node/node.dart';
 import 'package:luyen_tap/data_structure_and_algorithms/queue/queue.dart';
+import 'package:luyen_tap/data_structure_and_algorithms/node/filter_field.dart';
 
 void main(List<String> arguments) {
   // luyen_tap.restElement();
@@ -13,5 +15,25 @@ void main(List<String> arguments) {
   //   Duration(seconds: 0),
   //   () async => await luyen_tap.testString(),
   // );
-  print(decodeStringWithQueue("3[a]"));
+  // print(decodeStringWithQueue("3[a]"));
+  String name = "name";
+  String organization = "organization";
+  String position = "position";
+  String wifi = "wifi";
+  String location = "location";
+  String device = "device";
+  FilterField nameNode = FilterField(name: name);
+  FilterField organizationNode = FilterField(name: organization);
+  FilterField positionNode = FilterField(name: position);
+  FilterField wifiNode = FilterField(name: wifi, isFilter: false);
+  FilterField locationNode = FilterField(name: location);
+  FilterField deviceNode = FilterField(name: device, isFilter: false);
+  nameNode.next = organizationNode;
+  organizationNode.next = positionNode;
+  positionNode.next = wifiNode;
+  wifiNode.next = locationNode;
+  locationNode.next = deviceNode;
+  List<FilterField> list = getListFilterField(nameNode);
+
+  print(List.generate(list.length, (index) => list[index].name));
 }
